1365c5e0f7adca908f1c7b9ab02553bfbfe193a0
CristoferGZ <cristofer.gutierrez.cerda@alumnos.sip.cl>
2022-04-21 09:22:36 -0400

    first commit

---
+++ Gutierrez_cristofer_jugueteo.js
@@ -0,0 +1,8 @@
+
+function muestraSiElNiñoPuedeSubirLaMontañaRusa (entradaAlturaEdad){
+    //entradaAlturaEdad =60;
+    if (entradaAlturaEdad > 52)
+    console.log('subete, chico' );
+    else(entradaAlturaEdad< 52)
+    console.log('lo siento ,chico. tal vez el proximo año');
+}
